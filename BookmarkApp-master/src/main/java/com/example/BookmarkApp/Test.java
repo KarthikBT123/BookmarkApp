f1ebb5a253df7c01caa1a8270c6c8e6740055d01
Karthik BT <krishnakarthik.bt@gmail.com>
2025-07-25 09:51:01 -0500

    Restored App

--- BookmarkApp-master/src/main/java/com/example/BookmarkApp/Test.java
+++
@@ -1,7 +0,0 @@
-
-public class Test {
-    public static void main(String[] args) {
-        // This is the entry point of the Java application
-        System.out.println("Hello, World!");
-    }
-}
